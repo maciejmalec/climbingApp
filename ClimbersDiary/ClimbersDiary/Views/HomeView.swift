dbb01a73c4d9d376782bbe329b33a9a99957f069
maciejmalec <malecmaciej.mail@gmail.com>
2022-01-25 17:07:33 +0000

    route list displayed + some work on the route colour representation

--- ClimbersDiary/ClimbersDiary/Views/HomeView.swift
+++ ClimbersDiary/ClimbersDiary/Views/HomeView.swift
@@ -18,11 +18,20 @@ struct HomeView: View {
                 List {
                     ForEach(routes, id: \.self){ route in
                         NavigationLink(destination: RouteView(route: route)){
-                            Text(route.grade)
-                        }
+                            VStack{
+                                Text("Route number: " + String(route.routeNo))
+                                    
+                                    
+                                Text("Grade: " + route.grade)
+                                ProgressView("Progress…", value: 1, total: 3)
+                                    .accentColor(.green)
+                            }
+                            
+                        }.listRowBackground(getColor(color: route.colour))
+                        
 
                     }
-                }.navigationTitle("Added Meals")
+                }.navigationTitle("Routes")
                 .searchable(text: $searchTxt)
         }.onAppear(perform: fetch)
         .navigationViewStyle(StackNavigationViewStyle())
@@ -44,6 +53,36 @@ struct HomeView: View {
             return routes.filter{ $0.colour.contains(searchTxt)}
         }
     }
+    
+    func getColor(color: String) -> Color {
+        
+        switch color{
+        case "Orange":
+            return Color.orange
+        case "Green":
+            return Color.green
+        case "Blue":
+            return Color.blue
+        case "Purple":
+            return Color.purple
+        case "Black":
+            return Color.gray
+        case "Pink":
+            return Color.pink
+        case "Red":
+            return Color.red
+        case "Yellow":
+            return Color.yellow
+        case "Mint":
+            return Color.mint
+        case "Wood":
+            return Color.brown
+        default:
+            return Color.white
+        }
+        
+    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
