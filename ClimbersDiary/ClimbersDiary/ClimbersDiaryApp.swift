74d18f5a4cf43aec3cd63df108f8b947960d0867
maciejmalec <malecmaciej.mail@gmail.com>
2022-01-25 13:25:25 +0000

    empty xcode project

---
+++ ClimbersDiary/ClimbersDiary/ClimbersDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbersDiaryApp.swift
+//  ClimbersDiary
+//
+//  Created by Maciej Malec on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClimbersDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
